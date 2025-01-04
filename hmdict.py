c9f39ecbbc16acb842161fac3bfef9917f89952c
sugacuteic <zoetang7@outlook.com>
2025-01-04 18:05:13 +0000

    Add files via upload

---
+++ hmdict.py
@@ -0,0 +1,19 @@
+country = {'name': 'malaysia'
+}
+country['capital'] = 'kuala lumpur'
+country['currency'] = 'malaysia ringgit'
+country['language'] = 'malay'
+print(country)
+
+country2 = {'name': 'england'
+            }
+country2['capital'] = 'london'
+country2['currency'] = 'pound'
+country2['language'] = 'english'
+
+country3 = {'name' : 'america'
+}
+country3['capital'] = 'new york'
+country3['currency'] = 'dollar'
+country3['language'] = '(american) english'
+print(country, country2)
